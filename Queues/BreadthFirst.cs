b70d648ec3639606ec6524ac3ae616fc0f4d3f0d
Matt Scott <mscott232@gmail.com>
2017-11-12 16:24:49 -0600

    BreadthFirstSearch completed by tracing steps not yet tracked

---
+++ Queues/BreadthFirst.cs
@@ -0,0 +1,103 @@
+﻿/**
+* Stack – Methods to invoke on the constructed stack
+*
+* <pre>
+*
+* Assignment: #3
+* Course: ADEV-3001
+* Date Created: November 2, 2017
+* 
+* Revision Log
+* Who        When       Reason
+* --------- ---------- ----------------------------------
+*
+* </pre>
+*
+* @author Matt Scott
+* @version 1.0
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queues
+{
+    class BreadthFirst
+    {
+        private char[,] maze;
+        private Queue<Point> queue;
+        private bool result = false;
+
+        public BreadthFirst(char[,] maze)
+        {
+            this.maze = maze;
+            queue = new Queue<Point>();
+        }
+
+        public bool BreadthFirstSearch (int startingRow, int startingColumn)
+        {
+            char visitedMarker = 'V';
+
+            Point startingPoint = new Point(startingRow, startingColumn);
+            queue.Enqueue(startingPoint);
+
+            while(queue.GetSize() > 0 || result)
+            {
+                Point p = queue.Dequeue();
+                int row = p.GetRow();
+                int column = p.GetColumn();
+
+                maze[row, column] = visitedMarker;
+
+                if (maze[row + 1, column] == 'E' || maze[row + 1, column] == ' ')
+                {
+                    if (maze[row + 1, column] == 'E')
+                    {
+                        result = true;
+                        break;
+                    }
+
+                    queue.Enqueue(new Point(row + 1, column, row, column));
+                }
+
+                if (maze[row, column + 1] == 'E' || maze[row, column + 1] == ' ')
+                {
+                    if (maze[row, column + 1] == 'E')
+                    {
+                        result = true;
+                        break;
+                    }
+
+                    queue.Enqueue(new Point(row, column + 1, row, column));
+                }
+
+                if (maze[row, column - 1] == 'E' || maze[row, column - 1] == ' ')
+                {
+                    if (maze[row, column - 1] == 'E')
+                    {
+                        result = true;
+                        break;
+                    }
+
+                    queue.Enqueue(new Point(row, column - 1, row, column));
+                }
+
+                if (maze[row - 1, column] == 'E' || maze[row - 1, column] == ' ')
+                {
+                    if (maze[row - 1, column] == 'E')
+                    {
+                        result = true;
+                        break;
+                    }
+
+                    queue.Enqueue(new Point(row - 1, column, row, column));
+                }
+            }
+
+            return result;
+        }
+    }
+}
--- Queues/Program.cs
+++ Queues/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Queues
 {
@@ -10,6 +11,40 @@ namespace Queues
     {
         static void Main(string[] args)
         {
+            // Read all the lines in the text file and load them into a string array
+            //String[] lines = File.ReadAllLines(@"C:\Users\Matt\Documents\School\Term 5\Programming 4\Assignments\Assignment 3\maze.maze");
+            //String[] lines = File.ReadAllLines(@"C:\Users\Matt\Documents\School\Term 5\Programming 4\Assignments\Assignment 3\TestMazeNoExit.maze");
+            String[] lines = File.ReadAllLines(@"C:\Users\Matt\Documents\School\Term 5\Programming 4\Assignments\Assignment 3\TestMazeExit.maze");
+
+
+            int[] dimensions = lines[0].Split(' ').Select(n => Convert.ToInt32(n)).ToArray();
+            int[] startingPoint = lines[1].Split(' ').Select(n => Convert.ToInt32(n)).ToArray();
+
+            char[,] maze = new char[dimensions[0], dimensions[1]];
+
+            // Load the array from the lines in the text file
+            for (int i = 2; i < lines.Length; i++)
+            {
+                char[] items = lines[i].ToCharArray();
+
+                for (int j = 0; j < items.Length; j++)
+                {
+                    maze[i - 2, j] = items[j];
+                }
+            }
+
+            BreadthFirst breadthFirst = new BreadthFirst(maze);
+
+            if(breadthFirst.BreadthFirstSearch(startingPoint[0], startingPoint[1]))
+            {
+                Console.WriteLine("Success");
+            }
+            else
+            {
+                Console.WriteLine("Fail");
+            }
+
+            Console.ReadKey();
         }
     }
 }
